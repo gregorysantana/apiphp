8186da7fbb8e34b0103fbb9c41646beb4f246fc3
Gregory Santana <gregorymhk@gmail.com>
2022-12-11 14:04:10 -0400

    Create handleApi.php

---
+++ handleApi.php
@@ -0,0 +1,29 @@
+<?php
+// Función para manejar una API
+function handleApi($url, $method, $data = array()) {
+  // Inicializar cURL
+  $ch = curl_init();
+
+  // Configurar la URL de la API
+  curl_setopt($ch, CURLOPT_URL, $url);
+
+  // Configurar el método de solicitud (POST, GET, PUT, etc.)
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+
+  // Configurar los datos a enviar a la API (si se especifican)
+  if (!empty($data)) {
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  }
+
+  // Configurar para que la función devuelva la respuesta en lugar de mostrarla directamente
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+  // Ejecutar la solicitud cURL
+  $response = curl_exec($ch);
+
+  // Cerrar la sesión cURL
+  curl_close($ch);
+
+  // Devolver la respuesta de la API
+  return $response;
+}
